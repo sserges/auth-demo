02dfa8cc946a3e33cf5a9263c3913e593357b590
sserges <agounkeserge@gmail.com>
2019-11-05 19:27:51 +0000

    Showing or Hiding Elements

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -26,7 +27,20 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return false;
+    return tokenNotExpired();
+    // const jwtHelper = new JwtHelper();
+    // const token = localStorage.getItem('token');
+
+    // if (!token) {
+    //   return false;
+    // }
+
+    // const expirationDate = jwtHelper.getTokenExpirationDate(token);
+    // const isExpired = jwtHelper.isTokenExpired(token);
+
+    // console.log('Expiration ', expirationDate);
+    // console.log('isExpired ', isExpired);
+    // return !isExpired;
   }
 }
 
