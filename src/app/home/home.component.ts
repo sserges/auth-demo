6307a3b2e41ad81999d821e94d4a43ba77a73341
sserges <agounkeserge@gmail.com>
2019-11-05 18:52:58 +0000

    Implementing Logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from './../services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
